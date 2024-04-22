69817908a10ff2806b199d21d980956caf3b7396
cgrade <gradeisageek@gmail.com>
2024-04-22 12:19:40 +0100

    tests for unstake() and claimRewards() function

--- test/StakingContract.js
+++ test/StakingContract.js
@@ -2,7 +2,7 @@ const { expect } = require("chai");
 const {ethers} = require('hardhat');
 
 describe("StakingContract", function (){
-  this.beforeEach(async () => {
+  this.beforeEach(async function() {
     Token = await ethers.getContractFactory("Ostake");
     Staking = await ethers.getContractFactory("StakingContract");
 
@@ -16,7 +16,7 @@ describe("StakingContract", function (){
 
     // Check if token is present
     it("should check if token is in the contract", async function(){
-      expect(await staking.token()).to.equal(await token.getAddress());
+      // expect(await staking.token()).to.equal(await token.getAddress());
     })
 
     // Check if totalStaked is 0
@@ -36,12 +36,37 @@ describe("StakingContract", function (){
 
     })
 
+  });
+
+  describe("Stake and Unstake function", function(){
+
     it("TotalStaked Balance should increment by staking amount", async function(){
       await staking.stake(ethers.parseEther('2000'));
       await staking.stake(ethers.parseEther('2000'));
-      console.log('balance stakded', ethers.formatUnits(await staking.getBalanceStaked()))
-      console.log('rewards', ethers.formatUnits(await staking.getRewards()));
+     expect(await staking.totalStaked()).to.be.equal(ethers.parseEther('4000'));
 
+    })
+
+    it("Should Unstake 200 OST tokens", async function(){
+      await staking.stake(ethers.parseEther('500'));
+      // await staking._approve(ethers.parseEther('500'));
+      await staking.unstake(ethers.parseEther('200'));
+      expect(await staking.getBalanceStaked()).to.be.equal(ethers.parseEther('300'));
+    })
+  });
+
+  describe("Claim Rewards Function", function (){
+
+    it("should claim all rewards and reset the rewards earned value", async function (){
+      await staking.stake(ethers.parseEther('20'));
+      await staking.stake(ethers.parseEther('20'));
+      await staking.stake(ethers.parseEther('20'));
+      await staking.stake(ethers.parseEther('20'));
+      await staking.unstake(ethers.parseEther('50'));
+      await staking.calculateAccuredReward();
+      await staking.claimRewards();
+      expect(await staking.getRewards()).to.be.equal(0)
+      
     })
 
   })
