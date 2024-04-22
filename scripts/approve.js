64192c4dd5b348623abd4884dacf2fe5e0903dc4
cgrade <gradeisageek@gmail.com>
2024-04-22 12:20:16 +0100

    scripts for interacting with the blockchain

--- scripts/approve.js
+++ scripts/approve.js
@@ -1,8 +1,11 @@
 const {ethers} = require('hardhat');
 
 async function main(){
-    const Ostake = await ethers.getContractAt('Ostake', '0x0e9E606349443b533347D443bA3E00FDa4D93690', await ethers.getSigners()[1]);
-    console.log(await Ostake.approve('0x17a405D74A3EA82123DB52590a8aE729827Bb3d8', ethers.parseEther('50000000')));
+    const Ostake = await ethers.getContractAt('Ostake', '0x9d4454B023096f34B160D6B654540c56A1F81688', await ethers.getSigners()[0]);
+    console.log(await Ostake.approve('0x5eb3Bc0a489C5A8288765d2336659EbCA68FCd00', ethers.parseEther('50000000')));
+    const Staking = await ethers.getContractAt('StakingContract', '0x5eb3Bc0a489C5A8288765d2336659EbCA68FCd00', await ethers.getSigners()[0]);
+    console.log(await Staking.approve(ethers.parseEther('200')))
+    // console.log(await Ostake.approve(await ethers.getSigners()[1], ethers.parseEther('500')));
     console.log('done')
 
 }
--- scripts/interact.js
+++ scripts/interact.js
@@ -1,21 +1,27 @@
 const { ethers } = require('hardhat');
 
 async function main(){
-    const owner = await ethers.getSigners()[1];
-    const StakingContract = await ethers.getContractAt("StakingContract", "0x17a405D74A3EA82123DB52590a8aE729827Bb3d8", owner );
+    const owner = await ethers.getSigners()[0];
+    const StakingContract = await ethers.getContractAt("StakingContract", "0x5eb3Bc0a489C5A8288765d2336659EbCA68FCd00", owner );
 
-    const Ostake = await ethers.getContractAt("Ostake", "0x0e9E606349443b533347D443bA3E00FDa4D93690", owner);
+    const Ostake = await ethers.getContractAt("Ostake", "0x9d4454B023096f34B160D6B654540c56A1F81688", owner);
 
 
     // await StakingContract.trf();
     console.log('Balance of Staking Contract:', ethers.formatUnits(await Ostake.balanceOf(StakingContract.getAddress())));
-    // await StakingContract.stake(ethers.parseEther("2000"));
-    // console.log('Just staked 2000');
+    // await StakingContract.stake(ethers.parseEther("20"));
+    // console.log('Just staked 20');
     // console.log('Balance of Staking Contract After Staking:', ethers.formatUnits(await Ostake.balanceOf(StakingContract.getAddress())));
     await StakingContract.calculateAccuredReward();
     console.log('rewards for user', `${ethers.formatUnits(await StakingContract.getRewards())}OST`);
     console.log('total staked in the contract', ethers.formatUnits(await StakingContract.totalStaked()));
     console.log('balance of OST token staked', ethers.formatUnits(await StakingContract.getBalanceStaked()));
+    await StakingContract.unstake(ethers.parseEther('10'));
+
+    console.log('Just unstaked 20 OST tokens');
+    console.log('Balance left after unstaking', await StakingContract.getBalanceStaked());
+    // console.log(owner)
+
     // console.log(await StakingContract.())
     // console.log(await StakingContract.d)
 
